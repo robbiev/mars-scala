1570269eac9c02a9989607752d8bf757202fad9b
Robbie Vanbrabant <robbie.vanbrabant@gmail.com>
2013-06-29 11:49:23 +0100

    better responsibilities

--- src/Program.scala
+++ src/Program.scala
@@ -10,80 +10,40 @@ object MarsChars {
   }
 }
 
-object Program extends App {
-  import MarsChars._
-  val world = World(5,5)
-  val rover = Rover(Point(1, 2), 'N'.toOrientation, world)
-  commandsFromString("LMLMLMLMM") map rover.processCommand
-  println(rover)
-  
-  val rover2 = Rover(Point(3, 3), 'E'.toOrientation, world)
-  commandsFromString("MMRMMRMRRM") map rover2.processCommand
-  println(rover2)
-  /* 
-    1 3 N
-    5 1 E
-   */
-  
-  def commandsFromString(commands: String) = {
-    commands map {
-      case Left.letter => Left
-      case Right.letter => Right
-      case Move.letter => Move
-    } 
-  }
-}
+sealed abstract class Command(val letter: Char)
+case object Move extends Command('M')
+case object Left extends Command('L')
+case object Right extends Command('R')
 
-sealed trait Command {
-  val letter: Char
-}
-case object Move extends Command {
-  val letter = 'M'
-}
-case object Left extends Command {
-  val letter = 'L'
-}
-case object Right extends Command {
-  val letter = 'R'
-}
 object Orientation {
-  val ORDER = North :: East ::South :: West :: Nil
+  private val ORDER = Map(North.ordinal -> North,
+    East.ordinal -> East,
+    South.ordinal -> South,
+    West.ordinal -> West)
 }
-sealed trait Orientation {
-  val letter: Char
+sealed abstract class Orientation(val letter: Char, val ordinal: Int) {
+  def right = move(_ + 1)
+  def left = move(_ - 1)
+  def move(func: Int => Int) = {
+	val newIndex = (Orientation.ORDER.size + func(this.ordinal)) % Orientation.ORDER.size
+	Orientation.ORDER(newIndex)
+  }
 }
-case object North extends Orientation {
-  val letter = 'N'
-}
-case object West extends Orientation {
-  val letter = 'W'
-}
-case object South extends Orientation {
-  val letter = 'S'
-}
-case object East extends Orientation {
-  val letter = 'E'
-}
+case object North extends Orientation('N', 0)
+case object West extends Orientation('W', 3)
+case object South extends Orientation('S', 2)
+case object East extends Orientation('E', 1)
 case class Point(x: Int, y: Int)
-case class Rover(var position: Point, var direction: Orientation, world: World) {
-  
-  def mutateDirection(func: Int => Int): Unit = {
-	val newIndex = (Orientation.ORDER.length + func(Orientation.ORDER.indexOf(direction))) % Orientation.ORDER.length
-	direction = Orientation.ORDER(newIndex)
-  }
-  
-  def move(): Unit = {
-    position = world.moveInWorld(position, direction)
-  }
-  
+
+case class Rover(var position: Point, var orientation: Orientation, world: World) {
   def processCommand(command: Command) = command match {
-    case Left => mutateDirection(world.dec)
-    case Right => mutateDirection(world.inc)
-    case Move => move()
+    case Left => orientation = orientation.left
+    case Right => orientation = orientation.right 
+    case Move => position = world.moveInWorld(position, orientation)
   }
   
   override def toString() = {
-    s"${position.x} ${position.y} $direction"
+    s"${position.x} ${position.y} $orientation"
   }
 }
 
@@ -91,9 +51,9 @@ case class World(topX: Int, topY: Int) {
   val bottomX = 0
   val bottomY = 0
 
-  def moveInWorld(position: Point, direction: Orientation): Point = {
+  def moveInWorld(position: Point, orientation: Orientation): Point = {
     val Point(x, y) = position
-    direction match {
+    orientation match {
       case North => position.copy(y = if (y < topY) inc(y) else y)
       case South => position.copy(y = if (y > bottomY) dec(y) else y)
       case East => position.copy(x = if (x < topX) inc(x) else x)
@@ -103,4 +63,27 @@ case class World(topX: Int, topY: Int) {
 
   def inc(i: Int) = i + 1
   def dec(i: Int) = i - 1
+}
+
+object Program extends App {
+  import MarsChars._
+  val world = World(5,5)
+
+  val rover = Rover(Point(1, 2), 'N'.toOrientation, world)
+  commandsFromString("LMLMLMLMM") map rover.processCommand
+  assert(Point(1,3) == rover.position)
+  assert(North == rover.orientation)
+  
+  val rover2 = Rover(Point(3, 3), 'E'.toOrientation, world)
+  commandsFromString("MMRMMRMRRM") map rover2.processCommand
+  assert(Point(5,1) == rover2.position)
+  assert(East == rover2.orientation)
+  
+  def commandsFromString(commands: String) = {
+    commands map {
+      case Left.letter => Left
+      case Right.letter => Right
+      case Move.letter => Move
+    } 
+  }
 }
